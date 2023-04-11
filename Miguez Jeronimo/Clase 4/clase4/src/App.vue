9b4030670ef282c5396aaedbee43e18e30ce8f94
jeromiguez <jeromiguez@gmail.com>
2023-04-11 20:43:52 -0300

    subimos el ejercicio de clase 4

---
+++ Miguez Jeronimo/Clase 4/clase4/src/App.vue
@@ -0,0 +1,76 @@
+<script setup>
+import Header from './components/Header.vue'
+import Instrumentos from './components/Instrumentos.vue'
+import AgregarInstrumento from './components/AgregarInstrumento.vue'
+</script>
+
+<template>
+  <header>
+    <!--<img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+    -->
+    <Header/> 
+  </header>
+
+  <main>
+    <TheWelcome />
+    <Instrumentos v-bind:instrumento="instrumento" @borrarInstrumento="borrarInstrumento"/>
+    <AgregarInstrumento v-bind:instrumento="instrumento" @agregarInst="agregarInstrumento" />
+  </main>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      instrumento: [
+        {id: 1, tipo: "Guitarra", modelo: "Gibson"},
+        {id: 2, tipo: "Piano", modelo: "Digital"},
+        {id: 3, tipo: "Violin", modelo: "Cuerdas"},
+        {id: 4, tipo: "Piano", modelo: "Cola"}
+      ]
+    }
+  },
+  methods: {
+    agregarInstrumento(newInst) {
+      this.instrumento.push(newInst);
+    },
+    borrarInstrumento(i) {
+      this.instrumento.splice(i, 1);
+    }
+    
+  }
+};
+</script>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ Miguez Jeronimo/Clase 4/clase4/src/components/Instrumentos.vue
@@ -0,0 +1,29 @@
+<template>
+<table>
+<tr v-for="instrumento in instrumento">
+    <td>{{instrumento.id}}</td>
+    <td>{{instrumento.tipo}}</td>
+    <td>{{instrumento.modelo}}</td>
+    <td>
+        <button @click="borrarInstrumento(instrumento.id)">Eliminar</button>
+    </td>
+</tr>
+</table>
+
+</template>
+
+<script>
+export default {
+    props: ['instrumento'],
+    emits: ['borrarInstrumento'],
+    methods: {
+        borrarInstrumento(id) {
+            const index = this.instrumento.findIndex(instrumento => id ==instrumento.id)
+            this.$emit('borrarInstrumento', index);
+
+
+        }
+    }
+};
+
+</script>
